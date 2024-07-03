86c225bed823b2abc7fb2d72446f5ba1825f96dd
ValeriTodorov123 <valerytodorov5@gmail.com>
2024-07-03 16:26:19 +0300

    eink example code

--- Embedded/Epaper/Epaper.ino
+++ Embedded/Epaper/Epaper.ino
@@ -1,37 +1,24 @@
-// include library, include base class, make path known
 #include <GxEPD.h>
-
-// select the display class to use, only one
 #include <GxGDEH0154D67/GxGDEH0154D67.h>  // 1.54" b/w 200x200, SSD1681
-
 #include GxEPD_BitmapExamples
-
-// FreeFonts from Adafruit_GFX
-// #include <Fonts/FreeMonoBold9pt7b.h>
 #include <Fonts/FreeSansBold18pt7b.h>
-// #include <Fonts/FreeMonoBold18pt7b.h>
-// #include <Fonts/FreeMonoBold24pt7b.h>
-
 #include <GxIO/GxIO_SPI/GxIO_SPI.h>
 #include <GxIO/GxIO.h>
 
-// for SPI pin definitions see e.g.:
-// C:\Users\xxx\Documents\Arduino\hardware\espressif\esp32\variants\lolin32\pins_arduino.h
-
-GxIO_Class io(SPI, /*CS=5*/ SS, /*DC=*/ 17, /*RST=*/ 16); // arbitrary selection of 17, 16
-GxEPD_Class display(io, /*RST=*/ 16, /*BUSY=*/ 4); // arbitrary selection of (16), 4
+GxIO_Class io(SPI, /*CS=5*/ SS, /*DC=*/ 17, /*RST=*/ 16);
+GxEPD_Class display(io, /*RST=*/ 16, /*BUSY=*/ 4);
 
 void setup() {
   Serial.begin(115200);
   Serial.println();
   Serial.println("setup");
 
-  display.init(115200); // enable diagnostic output on Serial
+  display.init(115200); 
   drawHelloWorld();
   display.update();
 
   Serial.println("setup done");
-
+  
   // display.powerDown(); // Ne se chete s tova
 }
 
